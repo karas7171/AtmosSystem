e73b1f06827a713a7d5b7f9ff8d2b10db86466bf
karas7171 <romanrazoritelev936@gmail.com>
2026-01-15 20:32:23 +0300

    Рефакторинг AtmosSystem на 1D массив

--- src/main/java/com/karas7171/atmossystem/atmos/AtmosManager.java
+++ src/main/java/com/karas7171/atmossystem/atmos/AtmosManager.java
@@ -8,16 +8,21 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AirBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AtmosManager {
     private static AtmosManager instance;
-    private Level currentScanLevel = null;
 
-    private final Map<BlockPos, GasCell> cells = new HashMap<>();
+    private int minX, minZ, minY;
+    private int maxX, maxZ, maxY;
+    private int sizeX, sizeZ, sizeY;
+
+    private float[] pressure;
+    private float[] temperature;
+    private float[] oxygen;
+    private boolean[] solid;
+
+    List<BlockPos> airBlocks = new ArrayList<>();
 
     public static AtmosManager get() {
         if (instance == null) {
@@ -27,143 +32,177 @@ public class AtmosManager {
     }
 
     public void scanFullZone(Level level, BlockPos startPos) {
-        this.currentScanLevel = level;
 
-        if (isAirBlock(startPos)) {
-            registerCell(startPos);
+        if (isAirBlock(level, startPos)) {
+            registerAirBLock(startPos);
 
-            scanYLine(startPos);
-            scanZLine(startPos);
-            scanXLine(startPos);
+            scanYLine(level, startPos);
+            scanZLine(level, startPos);
+            scanXLine(level, startPos);
 
-            List<BlockPos> xLineBlocks = findXLineBlocks(startPos);
+            List<BlockPos> xLineBlocks = findXLineBlocks(level, startPos);
             List<BlockPos> zLineForXBlock = new ArrayList<>();
 
             for (BlockPos xPos : xLineBlocks) {
-                scanYLine(xPos);
-                scanZLine(xPos);
-                zLineForXBlock.addAll(findZlineBlocks(xPos));
+                scanYLine(level, xPos);
+                scanZLine(level, xPos);
+                zLineForXBlock.addAll(findZlineBlocks(level, xPos));
             }
 
             for (BlockPos zPos : zLineForXBlock) {
-                scanYLine(zPos);
+                scanYLine(level, zPos);
             }
         }
-
-        this.currentScanLevel = null;
+        createArraysFromAirBlocks(airBlocks);
     }
 
-    private boolean isAirBlock(BlockPos pos) {
-        if (currentScanLevel == null || !currentScanLevel.isLoaded(pos)) return false;
-        BlockState state = currentScanLevel.getBlockState(pos);
+    private void createArraysFromAirBlocks(List<BlockPos> airBlocks) {
+        sizeX = maxX - minX + 1;
+        sizeY = maxY - minY + 1;
+        sizeZ = maxZ - minZ + 1;
+        int totalCell = sizeX * sizeY * sizeZ;
+
+        pressure = new float[totalCell];
+        temperature = new float[totalCell];
+        oxygen = new float[totalCell];
+        solid = new boolean[totalCell]; // true - стена
+
+        Arrays.fill(solid, true);
+
+        for (BlockPos pos : airBlocks) {
+            int index = convertToIndex(pos, sizeY, sizeZ);
+            solid[index] = false;
+            registerCell(index);
+        }
+    }
+
+    private boolean isAirBlock(Level level, BlockPos pos) {
+        if (level == null || !level.isLoaded(pos)) return false;
+        BlockState state = level.getBlockState(pos);
         return state.isAir();
     }
 
-    private void scanYLine(BlockPos startPos) {
+    private void scanYLine(Level level, BlockPos startPos) {
         BlockPos buffer = startPos.above();
-        while (isAirBlock(buffer)) {
-            registerCell(buffer);
+        while (isAirBlock(level, buffer)) {
+            registerAirBLock(buffer);
             buffer = buffer.above();
         }
 
         buffer = startPos.below();
-        while (isAirBlock(buffer)) {
-            registerCell(buffer);
+        while (isAirBlock(level, buffer)) {
+            registerAirBLock(buffer);
             buffer = buffer.below();
         }
     }
 
-    private void scanZLine(BlockPos startPos) {
-        BlockPos buffer = startPos.north(); // z - 1;
-        while (isAirBlock(buffer)) {
-            registerCell(buffer);
-            buffer = buffer.north();
+    private void scanZLine(Level level, BlockPos startPos) {
+        BlockPos buffer = startPos.south(); // z + 1
+        while (isAirBlock(level, buffer)) {
+            registerAirBLock(buffer);
+            buffer = buffer.south();
         }
 
-        buffer = startPos.south(); // z + 1
-        while (isAirBlock(buffer)) {
-            registerCell(buffer);
-            buffer = buffer.south();
+        buffer = startPos.north(); // z - 1
+        while (isAirBlock(level, buffer)) {
+            registerAirBLock(buffer);
+            buffer = buffer.north();
         }
     }
 
-    private List<BlockPos> findZlineBlocks(BlockPos startPos) {
+    private List<BlockPos> findZlineBlocks(Level level, BlockPos startPos) {
         List<BlockPos> foundblocks = new ArrayList<>();
 
         BlockPos buffer = startPos.north();
-        while (isAirBlock(buffer)) {
+        while (isAirBlock(level, buffer)) {
             foundblocks.add(buffer);
             buffer = buffer.north();
         }
 
         buffer = startPos.south();
-        while (isAirBlock(buffer)) {
+        while (isAirBlock(level, buffer)) {
             foundblocks.add(buffer);
             buffer = buffer.south();
         }
         return  foundblocks;
     }
 
-    private void scanXLine(BlockPos startPos) {
-        BlockPos buffer = startPos.west(); // x - 1
-        while (isAirBlock(buffer)) {
-            registerCell(buffer);
-            buffer = buffer.west();
+    private void scanXLine(Level level, BlockPos startPos) {
+        BlockPos buffer = startPos.east(); // x + 1
+        while (isAirBlock(level, buffer)) {
+            registerAirBLock(buffer);
+            buffer = buffer.east();
         }
 
-        buffer = startPos.east(); // x + 1
-        while (isAirBlock(buffer)) {
-            registerCell(buffer);
-            buffer = buffer.east();
+        buffer = startPos.west(); // x - 1
+        while (isAirBlock(level, buffer)) {
+            registerAirBLock(buffer);
+            buffer = buffer.west();
         }
     }
 
-    private List<BlockPos> findXLineBlocks(BlockPos startPos) {
+    private List<BlockPos> findXLineBlocks(Level level, BlockPos startPos) {
         List<BlockPos> foundBlocks = new ArrayList<>();
 
         BlockPos buffer = startPos.west();
-        while (isAirBlock(buffer)) {
+        while (isAirBlock(level, buffer)) {
             foundBlocks.add(buffer);
             buffer = buffer.west();
         }
 
         buffer = startPos.east();
-        while (isAirBlock(buffer)) {
+        while (isAirBlock(level, buffer)) {
             foundBlocks.add(buffer);
             buffer = buffer.east();
         }
 
         return foundBlocks;
     }
-
-    private void registerCell(BlockPos pos) {
-        GasCell cell = new GasCell();
-        cell.setPressure(101.325f);
-        cell.setTemperature(293.15f);
-        cell.setOxygen(41.57f);
-        cells.put(pos, cell);
+    private void registerAirBLock(BlockPos pos) {
+        airBlocks.add(pos);
+        updateBounds(pos);
     }
 
-    public GasCell getGasCellInfo(BlockPos pos) {
-        GasCell cellInfo = cells.get(pos);
+    private void updateBounds(BlockPos pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
 
-        return  cellInfo;
+        if (x < minX) minX = x;
+        if (x > maxX) maxX = x;
+        if (y < minY) minY = y;
+        if (y > maxY) maxY = y;
+        if (z < minZ) minZ = z;
+        if (z > maxZ) maxZ = z;
     }
 
+    private int convertToIndex(BlockPos pos, int sizeY, int sizeZ) {
+        int relX =  pos.getX() - minX;
+        int relY =  pos.getY() - minY;
+        int relZ = pos.getZ() - minZ;
+
+        return (relX * sizeY * sizeZ) + (relY * sizeZ) + relZ;
+    }
+
+    private void registerCell(int index) {
+        pressure[index] = 101.325f;
+        temperature[index] = 293.15f;
+        oxygen[index] = 41.57f;
+    }
     public void reportCellInfo (BlockPos pos, CommandSourceStack source) {
-        GasCell cellInfo = cells.get(pos);
         StringBuilder message = new StringBuilder();
 
         message.append("=----------=").append("\n");
         message.append("Позиция: ").append(pos.toShortString()).append("\n");
-        if (cellInfo == null) {
-            message.append("Газовый тайл отсутствует").append("\n");
+        if (pressure == null) {
+            message.append("Газовый тайл отсутствует");
             source.sendSuccess(() -> Component.literal(message.toString()), false);
             return;
         }
-        message.append("Давление: ").append(cellInfo.getPressure()).append(" кПа\n");
-        message.append("Температура: ").append(cellInfo.getTemperature()).append(" К\n");
-        message.append("Кислород: ").append(cellInfo.getOxygen()).append(" моль");
+        int index = convertToIndex(pos, sizeY, sizeZ);
+        message.append("Давление: ").append(pressure[index]).append(" кПа\n");
+        message.append("Температура: ").append(temperature[index]).append(" К\n");
+        message.append("Кислород: ").append(oxygen[index]).append(" моль");
         source.sendSuccess(() -> Component.literal(message.toString()), false);
     }
 }
