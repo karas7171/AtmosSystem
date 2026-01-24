9aa798f62ff4630663be73c0afb9d02c85886f82
karas7171 <romanrazoritelev936@gmail.com>
2026-01-24 19:06:55 +0300

    feat: add core atmospheric logic (BFS, Axial) and progress listener

--- src/main/java/com/karas7171/atmossystem/core/AtmosZone.java
+++ src/main/java/com/karas7171/atmossystem/core/AtmosZone.java
@@ -4,7 +4,6 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
